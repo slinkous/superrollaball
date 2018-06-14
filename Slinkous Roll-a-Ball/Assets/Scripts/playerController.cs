d6dc713f25aee5f869adba1114cf532946c539b7
Sarah Linkous <sralinkous@gmail.com>
2018-06-13 18:42:20 -0700

    Upload project

---
+++ Slinkous Roll-a-Ball/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	Rigidbody rb;
+	public float moveSpeed = 1000f;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+		offset = Camera.main.transform.position - this.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hdir = Input.GetAxisRaw ("Horizontal");
+		float vdir = Input.GetAxisRaw ("Vertical");
+		Vector3 directionVector = new Vector3 (hdir, 0, vdir);
+		Vector3 unitVector = directionVector.normalized;
+		Vector3 forceVector = unitVector * moveSpeed * Time.deltaTime;
+
+		rb.AddForce (forceVector);
+		Camera.main.transform.position = this.transform.position + offset;
+	}
+}
